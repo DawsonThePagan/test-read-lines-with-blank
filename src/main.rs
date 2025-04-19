4a603490b4b5ec87cbaf16164138b4c11f8348a8
Bailey Dawson <dawson270500@gmail.com>
2025-04-19 20:43:48 +0100

    Updated to test new function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use read_lines_with_blank::read_lines_with_blank;
+use read_lines_with_blank::*;
 
 fn main() {
     let lines = read_lines_with_blank("input.txt").unwrap();
 
+    for line in lines {
+        println!("{}", line);
+    }
+
+    let lines = read_lines_with_blank_from_str("line1\n\nline2\n\nline3").unwrap();
     for line in lines {
         println!("{}", line);
     }
